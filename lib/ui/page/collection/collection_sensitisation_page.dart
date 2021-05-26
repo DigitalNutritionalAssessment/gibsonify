9a8c49f9e47b4d545dfa38c60c1d9079ad39e76b
rodgersjuan <rodgersjuan@gmail.com>
2021-05-26 13:57:47 +0100

    Sensitisation stage fix field criteria

--- lib/ui/page/collection/collection_sensitisation_page.dart
+++ lib/ui/page/collection/collection_sensitisation_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 
-
 import 'package:flutter_uikit/model/consumption_data.dart';
 import 'package:flutter_uikit/ui/widgets/collection_question.dart';
 import 'package:flutter_uikit/ui/widgets/custom_time_picker.dart';
@@ -11,7 +10,6 @@ import 'package:flutter_uikit/ui/widgets/custom_time_picker.dart';
 import 'package:flutter_uikit/utils/form_strings.dart';
 
 class SensitisationVisitDataCard extends StatefulWidget {
-
   final InterviewData initialInterviewData;
   final VoidCallback navigatePageStateForward;
   final VoidCallback navigatePageStateSubmit;
@@ -19,7 +17,7 @@ class SensitisationVisitDataCard extends StatefulWidget {
   final ValueChanged<InterviewData> updatePageState;
   final bool enabled;
 
-  const SensitisationVisitDataCard ({
+  const SensitisationVisitDataCard({
     @required this.navigatePageStateForward,
     @required this.navigatePageStateSubmit,
     @required this.updatePageState,
@@ -29,11 +27,12 @@ class SensitisationVisitDataCard extends StatefulWidget {
   });
 
   @override
-  _SensitisationVisitDataCardState createState() => _SensitisationVisitDataCardState();
+  _SensitisationVisitDataCardState createState() =>
+      _SensitisationVisitDataCardState();
 }
 
-class _SensitisationVisitDataCardState extends State<SensitisationVisitDataCard> {
-
+class _SensitisationVisitDataCardState
+    extends State<SensitisationVisitDataCard> {
   InterviewData interviewData = new InterviewData(
     sensitizationVisitDate: DateTime.now(),
     respondent: Person(birthDate: DateTime.now()),
@@ -44,30 +43,29 @@ class _SensitisationVisitDataCardState extends State<SensitisationVisitDataCard>
   VoidCallback saveConsumptionData = () => {};
 
   @override
-  void initState(){
-    if (widget.initialInterviewData != null) interviewData = widget.initialInterviewData;
+  void initState() {
+    if (widget.initialInterviewData != null)
+      interviewData = widget.initialInterviewData;
     if (widget.enabled != null) enabled = widget.enabled;
-    if (widget.saveConsumptionData != null) saveConsumptionData = widget.saveConsumptionData;
+    if (widget.saveConsumptionData != null)
+      saveConsumptionData = widget.saveConsumptionData;
 
     getEnumeratorInfo();
     super.initState();
   }
 
-  void getEnumeratorInfo() async{
+  void getEnumeratorInfo() async {
 //    SharedPreferences _prefs = await SharedPreferences.getInstance();
 //    interviewData.enumerator.name = _prefs.getString("userName");
 //    interviewData.enumerator.employeeNumber = _prefs.getInt("employeeName");
-      Person enumerator = await Person.getEnumeratorFromSharedPrefs();
-      interviewData.enumerator = enumerator;
-      interviewData.location.locationName = await InterviewData.getLocationFromSharedPrefs();
-
+    Person enumerator = await Person.getEnumeratorFromSharedPrefs();
+    interviewData.enumerator = enumerator;
+    interviewData.location.locationName =
+        await InterviewData.getLocationFromSharedPrefs();
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-
     final FocusNode _hhFocusNode = FocusNode();
     final FocusNode _respNameFocusNode = FocusNode();
     final FocusNode _respID = FocusNode();
@@ -77,81 +75,101 @@ class _SensitisationVisitDataCardState extends State<SensitisationVisitDataCard>
       child: Column(
         children: <Widget>[
           Card(
-          elevation: 2.0,
-          child: Container(
-            child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    FormQuestion(
-                      questionText: "Household Identification",
-                      hint:"(10 characters)",
-                      validate: (answer) {
-                        if (answer.length > 10){
-                          return "10 characters only!";
-                        }
-                        return emptyFieldValidator(answer);
-                      },
-                      initialText: interviewData.householdIdentification ?? null,
-                      onSaved: (answer) {
-                        interviewData.householdIdentification = answer;
-                        widget.updatePageState(interviewData);
+            elevation: 2.0,
+            child: Container(
+              child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      FormQuestion(
+                        questionText: "Household Identification",
+                        hint: "(10 characters)",
+                        validate: (answer) {
+                          if (answer.length > 10) {
+                            return "10 characters only!";
+                          }
+                          return emptyFieldValidator(answer);
                         },
-                      focusNode: _hhFocusNode,
-                      nextFocusNode: _respNameFocusNode,
-                      enabled: enabled,
-                    ),
-                    FormQuestion(
-                      questionText: "Respondent Name",
-                      hint:"",
-                      validate: emptyFieldValidator,
-                      initialText: interviewData?.respondent?.name ?? null,
-                      onSaved: (answer) {
-                        interviewData.respondent.name = answer;
-                        widget.updatePageState(interviewData);
+                        initialText:
+                            interviewData.householdIdentification ?? null,
+                        onSaved: (answer) {
+                          interviewData.householdIdentification = answer;
+                          widget.updatePageState(interviewData);
                         },
-                      focusNode: _respNameFocusNode,
-                      nextFocusNode: _respID,
-                      enabled: enabled,
-                    ),
-                    FormQuestion(
-                      questionText: "Respondent ID",
-                      hint:"",
-                      validate: emptyFieldValidator,
-                      initialText: interviewData?.respondent?.id?.toString() ?? null,
-                      onSaved: (answer) {
-                        interviewData.respondent.id = int.tryParse(answer);
-                        widget.updatePageState(interviewData);
-                      },
-                      focusNode: _respID,
-                      nextFocusNode: _respTelNo,
-                      enabled: enabled,
-                      keyboardType: TextInputType.number,
-                      // ignore: deprecated_member_use
-                      inputFormatters: [WhitelistingTextInputFormatter.digitsOnly]
-                    ),
-                    FormQuestion(
-                      questionText: "Respondent Telephone Number",
-                      hint:"",
-                      initialText:interviewData?.respondent?.telephone ?? null,
-                      validate: emptyFieldValidator,
-                      onSaved: (answer) {
-                        interviewData.respondent.telephone = answer;
-                        widget.updatePageState(interviewData);
+                        focusNode: _hhFocusNode,
+                        nextFocusNode: _respNameFocusNode,
+                        enabled: enabled,
+                      ),
+                      FormQuestion(
+                        questionText: "Respondent Name",
+                        hint: "",
+                        validate: emptyFieldValidator,
+                        initialText: interviewData?.respondent?.name ?? null,
+                        onSaved: (answer) {
+                          interviewData.respondent.name = answer;
+                          widget.updatePageState(interviewData);
                         },
-                      focusNode: _respTelNo,
-                      enabled: enabled,
-                      keyboardType: TextInputType.number,
-                    ),
-                    TimePicker(
-                      initialTime: interviewData?.respondent?.birthDate ?? null,
-                      onChanged: (time) {
-                        interviewData.respondent.birthDate = time;
-                        widget.updatePageState(interviewData);
-                      },
-                      questionText: "Respondent Birthdate",
-                      timePickerType: TimePickerType.DATE,
-                    ),
+                        focusNode: _respNameFocusNode,
+                        nextFocusNode: _respID,
+                        enabled: enabled,
+                      ),
+                      FormQuestion(
+                          questionText: "Respondent ID",
+                          hint: "",
+                          validate: (answer) {
+                            if (answer.length > 4) {
+                              return "Must be between 2-4 characters";
+                            }
+                            if (answer.length < 2) {
+                              return "Must be between 2-4 characters";
+                            }
+                            return emptyFieldValidator(answer);
+                          },
+                          initialText:
+                              interviewData?.respondent?.id?.toString() ?? null,
+                          onSaved: (answer) {
+                            interviewData.respondent.id = int.tryParse(answer);
+                            widget.updatePageState(interviewData);
+                          },
+                          focusNode: _respID,
+                          nextFocusNode: _respTelNo,
+                          enabled: enabled,
+                          keyboardType: TextInputType.number,
+                          // ignore: deprecated_member_use
+                          inputFormatters: [
+                            // ignore: deprecated_member_use
+                            WhitelistingTextInputFormatter.digitsOnly
+                          ]),
+                      FormQuestion(
+                        questionText: "Respondent Telephone Number",
+                        hint: "(+91)",
+                        initialText:
+                            interviewData?.respondent?.telephone ?? null,
+                        validate: (answer) {
+                          if (answer.length != 10) {
+                            return "Please enter a valid phone number";
+                            //Indian phone numbers are in the form of (+91) then ten digits
+                          }
+                          return emptyFieldValidator(answer);
+                        },
+                        onSaved: (answer) {
+                          interviewData.respondent.telephone = answer;
+                          widget.updatePageState(interviewData);
+                        },
+                        focusNode: _respTelNo,
+                        enabled: enabled,
+                        keyboardType: TextInputType.number,
+                      ),
+                      TimePicker(
+                        initialTime:
+                            interviewData?.respondent?.birthDate ?? null,
+                        onChanged: (time) {
+                          interviewData.respondent.birthDate = time;
+                          widget.updatePageState(interviewData);
+                        },
+                        questionText: "Respondent Birthdate",
+                        timePickerType: TimePickerType.DATE,
+                      ),
 //                    TimePicker(
 //                      initialTime: interviewData?.interviewStart ?? null,
 //                      onChanged: (time) {
@@ -161,28 +179,29 @@ class _SensitisationVisitDataCardState extends State<SensitisationVisitDataCard>
 //                      questionText: "Interview Start Time",
 //                      timePickerType: TimePickerType.DATETIME,
 //                    ),
-                    TimePicker(
-                      initialTime: interviewData?.sensitizationVisitDate ?? null,
-                      onChanged: (time) {
-                        interviewData.sensitizationVisitDate = time;
-                        widget.updatePageState(interviewData);
-                      },
-                      questionText: "Sensitization Visit Date",
-                      timePickerType: TimePickerType.DATE,
-                    ),
-                  ],
-                )
-              ),
+                      TimePicker(
+                        initialTime:
+                            interviewData?.sensitizationVisitDate ?? null,
+                        onChanged: (time) {
+                          interviewData.sensitizationVisitDate = time;
+                          widget.updatePageState(interviewData);
+                        },
+                        questionText: "Sensitization Visit Date",
+                        timePickerType: TimePickerType.DATE,
+                      ),
+                    ],
+                  )),
             ),
           ),
-          (enabled? buttons(): Container()),   // Display the buttons if enabled is true
-
+          (enabled
+              ? buttons()
+              : Container()), // Display the buttons if enabled is true
         ],
       ),
     );
-    }
+  }
 
-  Widget buttons(){
+  Widget buttons() {
     return new FittedBox(
       fit: BoxFit.contain,
       child: ButtonBar(
@@ -216,9 +235,6 @@ class _SensitisationVisitDataCardState extends State<SensitisationVisitDataCard>
         ],
       ),
     );
-
-
-
 
 //      RaisedButton(
 //      child: const Text('Go to First Pass'),
