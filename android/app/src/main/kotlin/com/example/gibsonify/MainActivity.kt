7a350ce5d25228a521b7b6affa386b136b50d6e3
Adam Sroka <45038919+adam-sroka@users.noreply.github.com>
2021-09-13 17:20:00 +0200

    refactor: start a fresh flutter project
    Ditch the outdated Flutter UI kit that this app has been based on and start rebuilding this project to be modular, with minimal dependencies, null safety, and working with Flutter 2.5.0 and newer.
    
    BREAKING CHANGE:

---
+++ android/app/src/main/kotlin/com/example/gibsonify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gibsonify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Greg Chu\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Greg Chu\new"
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/2.5.0/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/adamsroka/workspace/flutter_apps/gibsonify"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=2"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
