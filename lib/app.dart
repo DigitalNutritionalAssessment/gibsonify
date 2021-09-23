ec15515c4eb8342f9af79e6b6a5ed308151aa9c1
Adam Sroka <45038919+adam-sroka@users.noreply.github.com>
2021-09-23 03:00:17 +0200

    refactor: move app from main to separate file
    Move MaterialApp App from main.dart to a separate app.dart file.

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:gibsonify/navigation/navigation.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gibsonify',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      onGenerateRoute: ScreenRouter.route,
+    );
+  }
+}
