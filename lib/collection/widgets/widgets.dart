b508cbbc978bb98bbcf2a1249a131c95a2e9cbd2
Adam Sroka <45038919+adam-sroka@users.noreply.github.com>
2021-11-25 10:54:34 +0000

    refactor(collection): move second pass food card to its own file
    Move second pass cards to a standalone file and create a unique widget for them.

--- lib/collection/widgets/widgets.dart
+++ lib/collection/widgets/widgets.dart
@@ -4,3 +4,4 @@ export 'second_pass_screen.dart';
 export 'third_pass_screen.dart';
 export 'fourth_pass_screen.dart';
 export 'first_pass_food_item_card.dart';
+export 'second_pass_food_item_card.dart';
