b90e1293c58c6ae86a1720e85e180c5f7848898a
Adam Sroka <45038919+adam-sroka@users.noreply.github.com>
2022-01-04 22:43:38 +0100

    refactor(gibsonify_api): remove debugging print statements

--- packages/gibsonify_api/lib/src/gibsonify_api.dart
+++ packages/gibsonify_api/lib/src/gibsonify_api.dart
@@ -14,27 +14,17 @@ class GibsonifyApi {
   /// Returns an empty list if no data is found or data cannot be decoded.
   List<GibsonsForm?> loadForms() {
     List<GibsonsForm?> gibsonsForms = [];
-    print('API: loading Forms from local storage!'); // TODO: delete
     String? gibsonsFormsRaw = _sharedPreferences.getString(gibsonFormsKey);
     if (gibsonsFormsRaw == null || gibsonsFormsRaw.isEmpty == true) {
-      print(
-          'API: no data found in storage, returning an empty list'); // TODO: delete
       return [];
     } else {
-      print('API: found some data in storage, decoding'); // TODO: delete
-      print(gibsonsFormsRaw); // TODO: delete
       try {
         List<dynamic> partiallyDecodedGibsonsForms =
             jsonDecode(gibsonsFormsRaw);
-        print(
-            'API: Raw input decoded, proceeding to decode Forms'); // TODO: delete
         gibsonsForms = partiallyDecodedGibsonsForms
             .map((e) => GibsonsForm.fromJson(e))
             .toList();
-        print('API: decoded ${gibsonsForms.length} forms'); // TODO: delete
       } catch (e) {
-        print(
-            'API: Error decoding data from storage, returning an empty list'); // TODO: delete
         return [];
       }
       return gibsonsForms;
@@ -49,28 +39,19 @@ class GibsonifyApi {
     List<GibsonsForm?> gibsonsForms = loadForms();
 
     if (gibsonsForms.isEmpty) {
-      print(
-          'API: no valid Forms found in storage, saving the first Form'); // TODO: delete
       gibsonsForms = [gibsonsForm];
     } else {
-      print(
-          'API: Looking for match in ${gibsonsForms.length} forms'); // TODO: delete
       // Null assertion operator is used here since if gibsonsForms is not empty
       // then all its elements come from GibsonsForm.fromJson, so are not null.
       int gibsonsFormIndex =
           gibsonsForms.indexWhere((form) => form!.id == gibsonsForm.id);
       if (gibsonsFormIndex >= 0) {
-        print(
-            'API: match found at index $gibsonsFormIndex, rewriting with new Form'); // TODO: delete
         gibsonsForms[gibsonsFormIndex] = gibsonsForm;
       } else {
-        print('API: match not found, adding new Form'); // TODO: delete
         gibsonsForms.add(gibsonsForm);
       }
     }
     String gibsonsFormsEncoded = jsonEncode(gibsonsForms);
-    print(
-        'API: Forms successfuly jsonified, now saving to local storage!'); // TOOD: delete
     return _sharedPreferences.setString(gibsonFormsKey, gibsonsFormsEncoded);
   }
 
@@ -80,20 +61,12 @@ class GibsonifyApi {
   void deleteForm(String id) {
     List<GibsonsForm?> gibsonsForms = loadForms();
     if (gibsonsForms.isEmpty) {
-      print(
-          'API: no valid Forms found in storage, there is nothing to delete'); // TODO: delete
     } else {
-      print(
-          'API: Looking for match in ${gibsonsForms.length} forms'); // TODO: delete
       // Null assertion operator is used here just as in saveForm()
       int gibsonsFormIndex = gibsonsForms.indexWhere((form) => form!.id == id);
       if (gibsonsFormIndex >= 0) {
-        print(
-            'API: match found at index $gibsonsFormIndex, deleting this Form'); // TODO: delete
         gibsonsForms.removeAt(gibsonsFormIndex);
-      } else {
-        print('API: match not found, not deleting any Form'); // TODO: delete
-      }
+      } else {}
       String gibsonsFormsEncoded = jsonEncode(gibsonsForms);
       _sharedPreferences.setString(gibsonFormsKey, gibsonsFormsEncoded);
     }
