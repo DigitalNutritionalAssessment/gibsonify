ff5c3b5f4f6d7bf524f187fb9b08a1e5ab46e19b
Adam Sroka <45038919+adam-sroka@users.noreply.github.com>
2022-01-04 22:29:11 +0100

    feat(gibsonify_repository): add loading multiple forms and form deletion functionality

--- packages/gibsonify_repository/lib/src/gibsonify_repository.dart
+++ packages/gibsonify_repository/lib/src/gibsonify_repository.dart
@@ -9,16 +9,13 @@ class GibsonifyRepository {
   const GibsonifyRepository({required GibsonifyApi gibsonifyApi})
       : _gibsonifyApi = gibsonifyApi;
 
-  // TODO: Implement that if a Gibsons Form with same Id already exists it will
-  // be replaced, otherwise this form is appended to list of saved forms
   Future<void> saveForm(GibsonsForm gibsonsForm) =>
       _gibsonifyApi.saveForm(gibsonsForm);
 
-  // TODO: implement loading of multiple GibsonsForms - either have just one
-  // method loadForms that loads all of them, or also a method
-  // loadForm(String id) that only loads one form with matching UUID
-  GibsonsForm loadForm() => _gibsonifyApi.loadForm();
+  // TODO: investigate providing a Stream of forms instead
+  List<GibsonsForm?> loadForms() => _gibsonifyApi.loadForms();
 
-  // TODO: Implement deletion of Forms based on their UUID
-  // deleteForm(String id)
+  GibsonsForm loadForm() => _gibsonifyApi.loadForm(); // TODO: delete
+
+  void deleteForm(String id) => _gibsonifyApi.deleteForm(id);
 }
