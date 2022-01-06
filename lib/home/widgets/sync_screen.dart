57b44180fbc245a2e88ecc2f386a3b177ed73e69
Shazril Suhail <s.shazril@gmail.com>
2022-01-06 15:11:33 +0800

    feat:add collections export data

--- lib/home/widgets/sync_screen.dart
+++ lib/home/widgets/sync_screen.dart
@@ -4,44 +4,55 @@ import 'package:flutter_mailer/flutter_mailer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gibsonify/recipe/recipe.dart';
+import 'package:gibsonify/home/home.dart';
+import 'dart:convert';
 
 class SyncScreen extends StatelessWidget {
   const SyncScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<RecipeBloc, RecipeState>(
-        builder: (context, recipeState) {
-      String recipeJson = recipeState.toJson();
-      return Scaffold(
-          appBar: AppBar(title: const Text('Export Data via Email')),
-          body: Center(
-              child: ElevatedButton.icon(
-            onPressed: () async {
-              final directory = await getApplicationDocumentsDirectory();
-              final path = directory.path;
-              final _filePath = '$path/data.txt';
-              final _file = File(_filePath);
-              _file.writeAsString(recipeJson);
+    return BlocBuilder<HomeBloc, HomeState>(builder: (context, homeState) {
+      return BlocBuilder<RecipeBloc, RecipeState>(
+          builder: (context, recipeState) {
+        String recipeJson = recipeState.toJson();
+        String collectionJson = json.encode({
+          'collections':
+              homeState.gibsonsForms.map((x) => x!.toJson()).toList(),
+        });
+        return Scaffold(
+            appBar: AppBar(title: const Text('Export Data via Email')),
+            body: Center(
+                child: ElevatedButton.icon(
+              onPressed: () async {
+                final directory = await getApplicationDocumentsDirectory();
+                final path = directory.path;
 
-              final MailOptions mailOptions = MailOptions(
-                body:
-                    'Gibsonify collection and recipe data attached as a JSON string. <br> Data can be pasted into https://www.convertcsv.com/json-to-csv.htm to obtain a csv file.',
-                subject: 'Gibsonify collection and recipe data',
-                recipients: [],
-                isHTML: true,
-                bccRecipients: [],
-                ccRecipients: [],
-                attachments: [
-                  _filePath,
-                ],
-              );
+                final _recipefilePath = '$path/recipe_data.txt';
+                final _recipefile = File(_recipefilePath);
+                _recipefile.writeAsString(recipeJson);
 
-              await FlutterMailer.send(mailOptions);
-            },
-            icon: const Icon(Icons.send, size: 18),
-            label: const Text("Export saved data as JSON"),
-          )));
+                final _collectionfilePath = '$path/collection_data.txt';
+                final _collectionfile = File(_collectionfilePath);
+                _collectionfile.writeAsString(collectionJson);
+
+                final MailOptions mailOptions = MailOptions(
+                  body:
+                      'Gibsonify collection and recipe data attached as a JSON string. <br> Data can be pasted into https://www.convertcsv.com/json-to-csv.htm to obtain a csv file.',
+                  subject: 'Gibsonify collection and recipe data',
+                  recipients: [],
+                  isHTML: true,
+                  bccRecipients: [],
+                  ccRecipients: [],
+                  attachments: [_collectionfilePath, _recipefilePath],
+                );
+
+                await FlutterMailer.send(mailOptions);
+              },
+              icon: const Icon(Icons.send, size: 18),
+              label: const Text("Export saved data as JSON"),
+            )));
+      });
     });
   }
 }
