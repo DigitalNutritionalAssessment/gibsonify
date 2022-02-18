81a477163f91df77e6b7812882e01d25407674e5
Adam Sroka <45038919+adam-sroka@users.noreply.github.com>
2022-02-18 19:07:40 +0000

    fix: reset selected screen when opening collections
    Fix selectedScreenIndex not resetting upon loading an existing GibsonsForm or creating a new collection.

--- lib/home/widgets/collections_screen.dart
+++ lib/home/widgets/collections_screen.dart
@@ -39,6 +39,9 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
                                       GibsonsFormProvided(
                                           gibsonsForm:
                                               state.gibsonsForms[index]!));
+                                  context.read<CollectionBloc>().add(
+                                      const SelectedScreenIndexChanged(
+                                          changedSelectedScreenIndex: 0));
                                   Navigator.pushNamed(
                                       context, PageRouter.collection);
                                 },
@@ -57,6 +60,9 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
                                       GibsonsFormProvided(
                                           gibsonsForm:
                                               state.gibsonsForms[index]!));
+                                  context.read<CollectionBloc>().add(
+                                      const SelectedScreenIndexChanged(
+                                          changedSelectedScreenIndex: 0));
                                   Navigator.pushNamed(
                                       context, PageRouter.collection);
                                 },
@@ -93,6 +99,9 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
                         context
                             .read<CollectionBloc>()
                             .add(const GibsonsFormCreated());
+                        context.read<CollectionBloc>().add(
+                            const SelectedScreenIndexChanged(
+                                changedSelectedScreenIndex: 0));
                         Navigator.pushNamed(context, PageRouter.collection);
                       })
                 ]));
