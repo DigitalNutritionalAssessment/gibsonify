b2f425ba2b1eedb88509e61a17086cda3bed5331
Adam Sroka <git@adam.sr>
2022-03-03 19:37:02 +0000

    feat: update and simplify settings screen

--- lib/home/widgets/settings_screen.dart
+++ lib/home/widgets/settings_screen.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
-  @override
-  _SettingsScreenState createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Settings')),
-        body: const Center(child: Text('Various Settings')));
+        body: const Center(child: Text('No settings currently available')));
   }
 }
