e0ad424f6e82ebc41901f6e219d903eec024e81f
Adam Sroka <git@adam.sr>
2022-03-03 16:39:38 +0000

    feat: add long-press-to-delete option to third pass

--- lib/collection/widgets/third_pass_food_item_card.dart
+++ lib/collection/widgets/third_pass_food_item_card.dart
@@ -22,6 +22,15 @@ class ThirdPassFoodItemCard extends StatelessWidget {
   final VoidCallback? onMeasurementAdded;
   final ValueChanged<int>? onMeasurementDeleted;
 
+  void _onMeasurementDeleted(BuildContext context, int index) {
+    if (foodItem.measurements.length > 1) {
+      onMeasurementDeleted!(index);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Each food must have at least one measurement')));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -39,23 +48,14 @@ class ThirdPassFoodItemCard extends StatelessWidget {
                 padding: const EdgeInsets.only(bottom: 8.0),
                 itemCount: foodItem.measurements.length,
                 itemBuilder: (context, index) {
-                  // TODO: Try out a pop up button instead of Slidable
                   return Slidable(
                     key: Key(foodItem.measurements[index].id),
                     endActionPane: ActionPane(
                       motion: const ScrollMotion(),
                       children: [
                         SlidableAction(
-                          onPressed: (context) {
-                            if (foodItem.measurements.length > 1) {
-                              onMeasurementDeleted!(index);
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text(
-                                          'Each food must have at least one measurement')));
-                            }
-                          },
+                          onPressed: (context) =>
+                              _onMeasurementDeleted(context, index),
                           backgroundColor: const Color(0xFFFE4A49),
                           foregroundColor: Colors.white,
                           icon: Icons.delete,
@@ -63,87 +63,91 @@ class ThirdPassFoodItemCard extends StatelessWidget {
                         )
                       ],
                     ),
-                    child: Card(
-                      child: Column(
-                        children: [
-                          // TODO: fix RenderFlex overflow
-                          DropdownSearch<String>(
-                              dropdownSearchDecoration: InputDecoration(
-                                icon: const Icon(Icons.monitor_weight_outlined),
-                                labelText: "Measurement method",
-                                helperText: 'How the measurement is measured',
-                                // TODO: the errorText should be displayed if nothing is chosen
-                                // so investigate how this can be achieved with focusnodes or
-                                // mayge send an empty string (although that would not work all
-                                // the time)
+                    child: InkWell(
+                      onLongPress: () => _onMeasurementDeleted(context, index),
+                      child: Card(
+                        child: Column(
+                          children: [
+                            // TODO: fix RenderFlex overflow
+                            DropdownSearch<String>(
+                                dropdownSearchDecoration: InputDecoration(
+                                  icon:
+                                      const Icon(Icons.monitor_weight_outlined),
+                                  labelText: "Measurement method",
+                                  helperText: 'How the measurement is measured',
+                                  // TODO: the errorText should be displayed if nothing is chosen
+                                  // so investigate how this can be achieved with focusnodes or
+                                  // mayge send an empty string (although that would not work all
+                                  // the time)
+                                  errorText: !foodItem.measurements[index]
+                                          .isMethodValid()
+                                      ? 'Enter a measurement method'
+                                      : null,
+                                ),
+                                mode: Mode.MENU,
+                                showSelectedItems: true,
+                                showSearchBox: false,
+                                items: Measurement.measurementMethods,
+                                onChanged: (String? measurementMethod) =>
+                                    onMeasurementMethodChanged!({
+                                      'index': index,
+                                      'method': measurementMethod
+                                    }),
+                                selectedItem: foodItem
+                                    .measurements[index].measurementMethod),
+
+                            TextFormField(
+                              initialValue:
+                                  foodItem.measurements[index].measurementValue,
+                              decoration: InputDecoration(
+                                icon: const Icon(
+                                    Icons.drive_file_rename_outline_rounded),
+                                labelText: 'Measurement value',
+                                helperText: 'The amount or number you measured',
                                 errorText: !foodItem.measurements[index]
-                                        .isMethodValid()
-                                    ? 'Enter a measurement method'
+                                        .isValueValid()
+                                    // TODO: the errorText should be displayed if nothing is typed
+                                    // so investigate how this can be achieved with focusnodes
+                                    ? 'Enter the measured value in 1 to 4 digits'
                                     : null,
                               ),
-                              mode: Mode.MENU,
-                              showSelectedItems: true,
-                              showSearchBox: false,
-                              items: Measurement.measurementMethods,
-                              onChanged: (String? measurementMethod) =>
-                                  onMeasurementMethodChanged!({
-                                    'index': index,
-                                    'method': measurementMethod
-                                  }),
-                              selectedItem: foodItem
-                                  .measurements[index].measurementMethod),
-
-                          TextFormField(
-                            initialValue:
-                                foodItem.measurements[index].measurementValue,
-                            decoration: InputDecoration(
-                              icon: const Icon(
-                                  Icons.drive_file_rename_outline_rounded),
-                              labelText: 'Measurement value',
-                              helperText: 'The amount or number you measured',
-                              errorText: !foodItem.measurements[index]
-                                      .isValueValid()
-                                  // TODO: the errorText should be displayed if nothing is typed
-                                  // so investigate how this can be achieved with focusnodes
-                                  ? 'Enter the measured value in 1 to 4 digits'
-                                  : null,
+                              onChanged: (measurementValue) =>
+                                  onMeasurementValueChanged!({
+                                'index': index,
+                                'value': measurementValue
+                              }),
+                              textInputAction: TextInputAction.next,
                             ),
-                            onChanged: (measurementValue) =>
-                                onMeasurementValueChanged!({
-                              'index': index,
-                              'value': measurementValue
-                            }),
-                            textInputAction: TextInputAction.next,
-                          ),
 
-                          DropdownSearch<String>(
-                              dropdownSearchDecoration: InputDecoration(
-                                icon: const Icon(
-                                    Icons.radio_button_unchecked_outlined),
-                                labelText: "Measurement unit",
-                                helperText:
-                                    'The size of each measurement value',
-                                // TODO: the errorText should be displayed if nothing is chosen
-                                // so investigate how this can be achieved with focusnodes or
-                                // mayge send an empty string (although that would not work all
-                                // the time)
-                                errorText:
-                                    !foodItem.measurements[index].isUnitValid()
-                                        ? 'Select the measurement unit'
-                                        : null,
-                              ),
-                              mode: Mode.MENU,
-                              showSelectedItems: true,
-                              showSearchBox: false,
-                              items: Measurement.measurementUnits,
-                              onChanged: (String? measurementUnit) =>
-                                  onMeasurementUnitChanged!({
-                                    'index': index,
-                                    'unit': measurementUnit
-                                  }),
-                              selectedItem:
-                                  foodItem.measurements[index].measurementUnit),
-                        ],
+                            DropdownSearch<String>(
+                                dropdownSearchDecoration: InputDecoration(
+                                  icon: const Icon(
+                                      Icons.radio_button_unchecked_outlined),
+                                  labelText: "Measurement unit",
+                                  helperText:
+                                      'The size of each measurement value',
+                                  // TODO: the errorText should be displayed if nothing is chosen
+                                  // so investigate how this can be achieved with focusnodes or
+                                  // mayge send an empty string (although that would not work all
+                                  // the time)
+                                  errorText: !foodItem.measurements[index]
+                                          .isUnitValid()
+                                      ? 'Select the measurement unit'
+                                      : null,
+                                ),
+                                mode: Mode.MENU,
+                                showSelectedItems: true,
+                                showSearchBox: false,
+                                items: Measurement.measurementUnits,
+                                onChanged: (String? measurementUnit) =>
+                                    onMeasurementUnitChanged!({
+                                      'index': index,
+                                      'unit': measurementUnit
+                                    }),
+                                selectedItem: foodItem
+                                    .measurements[index].measurementUnit),
+                          ],
+                        ),
                       ),
                     ),
                   );
