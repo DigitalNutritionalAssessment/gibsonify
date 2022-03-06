352f36e8274bbf41b1ccdefd8eed55b6af046c6f
Adam Sroka <git@adam.sr>
2022-03-06 15:58:06 +0000

    feat(collection): change to dropdownsearch in time period
    Change FirstPassFoodItemCard to use DropdownSearch instead of DropdownButtonFormField, hence move to DropdownSearch completely in FirstPassScreen. This also changes the FoodItem.timePeriod values from just "Morning", "Afternoon", etc. to "Morning (3:01 am - 12:00 pm)", etc.

--- lib/collection/widgets/first_pass_food_item_card.dart
+++ lib/collection/widgets/first_pass_food_item_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 import 'package:gibsonify_api/gibsonify_api.dart';
 
@@ -19,15 +20,11 @@ class FirstPassFoodItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const List<DropdownMenuItem<String>> dropdownMenuItems = [
-      DropdownMenuItem(child: Text(''), value: ''),
-      DropdownMenuItem(
-          child: Text('Morning (3:01 am - 12:00 pm)'), value: 'Morning'),
-      DropdownMenuItem(
-          child: Text('Afternoon (12:01 pm - 4:00 pm)'), value: 'Afternoon'),
-      DropdownMenuItem(
-          child: Text('Evening (4:01 pm - 7:00 pm)'), value: 'Evening'),
-      DropdownMenuItem(child: Text('Night (7:01 pm - 3:00 am)'), value: 'Night')
+    const List<String> timePeriods = [
+      'Morning (3:01 am - 12:00 pm)',
+      'Afternoon (12:01 pm - 4:00 pm)',
+      'Evening (4:01 pm - 7:00 pm)',
+      'Night (7:01 pm - 3:00 am)'
     ];
 
     return Slidable(
@@ -64,19 +61,29 @@ class FirstPassFoodItemCard extends StatelessWidget {
                           : null),
                   onChanged: (value) => onNameChanged!(value),
                   textInputAction: TextInputAction.next),
-              DropdownButtonFormField(
-                  value: foodItem.timePeriod.value,
-                  decoration: InputDecoration(
+              DropdownSearch<String>(
+                  maxHeight: 225.0,
+                  dropdownSearchDecoration: InputDecoration(
                       icon: const Icon(Icons.access_time),
                       labelText: 'Time period',
                       helperText:
                           'Time period of consuming the food e.g. Morning',
+                      // TODO: the errorText should be displayed if nothing is chosen
+                      // so investigate how this can be achieved with focusnodes or
+                      // maybe send an empty string (although that would not work all
+                      // the time)
                       errorText: foodItem.timePeriod.invalid
                           ? 'Select a time period'
                           : null),
-                  items: dropdownMenuItems,
-                  onChanged: (String? value) =>
-                      onTimePeriodChanged!(value ?? '')),
+                  mode: Mode.MENU,
+                  showSelectedItems: true,
+                  showSearchBox: false,
+                  items: timePeriods,
+                  onChanged: (String? timePeriod) =>
+                      onTimePeriodChanged!(timePeriod ?? ''),
+                  // TODO: the selected item has to be a nullable string for the
+                  // dropdown field to display properly, fix this once we drop Formz
+                  selectedItem: foodItem.timePeriod.value)
             ],
           ),
         ),
