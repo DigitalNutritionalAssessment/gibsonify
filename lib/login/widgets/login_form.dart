3ee942a7980b9bec7482721e5809919a099e5c02
Shazril Suhail <s.shazril@gmail.com>
2022-03-15 18:18:48 +0000

    feat(login): add gibsonify logo to login screen

--- lib/login/widgets/login_form.dart
+++ lib/login/widgets/login_form.dart
@@ -13,61 +13,70 @@ class LoginForm extends StatelessWidget {
     return BlocBuilder<LoginBloc, LoginState>(
       builder: (context, state) {
         return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            key: Key(state.loginInfo.id),
-            children: [
-              TextFormField(
-                initialValue: state.loginInfo.employeeName,
-                decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'Employee Name',
-                    helperText: 'Enter your name',
-                    errorText:
-                        (isFieldModifiedAndEmpty(state.loginInfo.employeeName))
+          padding: const EdgeInsets.all(15.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                key: Key(state.loginInfo.id),
+                children: [
+                  Image.asset('assets/images/gibsonify_name_styled.png'),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    initialValue: state.loginInfo.employeeName,
+                    decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Employee Name',
+                        helperText: 'Enter your name',
+                        errorText: (isFieldModifiedAndEmpty(
+                                state.loginInfo.employeeName))
                             ? 'Name field cannot be empty'
                             : null),
-                onChanged: (value) {
-                  context
-                      .read<LoginBloc>()
-                      .add(EmployeeNameChanged(employeeName: value));
-                },
-                textInputAction: TextInputAction.next,
-                textCapitalization: TextCapitalization.sentences,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                initialValue: state.loginInfo.employeeId,
-                decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'Employee ID',
-                    helperText: 'Enter your employee ID',
-                    errorText:
-                        (isFieldModifiedAndEmpty(state.loginInfo.employeeId))
+                    onChanged: (value) {
+                      context
+                          .read<LoginBloc>()
+                          .add(EmployeeNameChanged(employeeName: value));
+                    },
+                    textInputAction: TextInputAction.next,
+                    textCapitalization: TextCapitalization.sentences,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    initialValue: state.loginInfo.employeeId,
+                    decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Employee ID',
+                        helperText: 'Enter your employee ID',
+                        errorText: (isFieldModifiedAndEmpty(
+                                state.loginInfo.employeeId))
                             ? 'Name field cannot be empty'
                             : null),
-                onChanged: (value) {
-                  context
-                      .read<LoginBloc>()
-                      .add(EmployeeIdChanged(employeeId: value));
-                },
-                textInputAction: TextInputAction.next,
-                textCapitalization: TextCapitalization.characters,
+                    onChanged: (value) {
+                      context
+                          .read<LoginBloc>()
+                          .add(EmployeeIdChanged(employeeId: value));
+                    },
+                    textInputAction: TextInputAction.next,
+                    textCapitalization: TextCapitalization.characters,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      child: const Text('Submit'),
+                      onPressed: () => {
+                            context
+                                .read<LoginBloc>()
+                                .add(const LoginInfoSaved()),
+                            Navigator.pushNamed(context, PageRouter.home)
+                          })
+                ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  child: const Text('Submit'),
-                  onPressed: () => {
-                        context.read<LoginBloc>().add(const LoginInfoSaved()),
-                        Navigator.pushNamed(context, PageRouter.home)
-                      })
-            ],
+            ),
           ),
         );
       },
