957e9d0f669f98d17e5bebef56e6ff745795c7cd
Adam Sroka <git@adam.sr>
2022-04-05 18:53:37 +0200

    refactor: move csv convertor functions to api

--- lib/import_export/bloc/import_export_bloc.dart
+++ lib/import_export/bloc/import_export_bloc.dart
@@ -42,8 +42,8 @@ class ImportExportBloc extends Bloc<ImportExportEvent, ImportExportState> {
     }
 
     String finishedGibsonsFormsCsv =
-        _convertFinishedGibsonsFormsToCsv(gibsonsForms);
-    String recipesCsv = _convertRecipesToCsv(recipes);
+        convertFinishedGibsonsFormsToCsv(gibsonsForms);
+    String recipesCsv = convertRecipesToCsv(recipes);
 
     try {
       if (!await Permission.storage.request().isGranted) {
@@ -103,8 +103,8 @@ class ImportExportBloc extends Bloc<ImportExportEvent, ImportExportState> {
     }
 
     String finishedGibsonsFormsCsv =
-        _convertFinishedGibsonsFormsToCsv(gibsonsForms);
-    String recipesCsv = _convertRecipesToCsv(recipes);
+        convertFinishedGibsonsFormsToCsv(gibsonsForms);
+    String recipesCsv = convertRecipesToCsv(recipes);
 
     try {
       final applicationDocumentsDirectory =
@@ -144,46 +144,4 @@ class ImportExportBloc extends Bloc<ImportExportEvent, ImportExportState> {
 
     emit(state.copyWith(dataShareStatus: DataShareStatus.notRequested));
   }
-}
-
-// TODO: move this function to the API
-String _convertFinishedGibsonsFormsToCsv(List<GibsonsForm?> gibsonsForms) {
-  String finishedGibsonsFormsCsvHeader =
-      'Collection ID,Employee Number,Household ID,Respondent Name,'
-      'Respondent Country Code,Respondent Tel Number Prefix,'
-      'Respondent Tel Number,Sensitization Date,Recall Day,'
-      'Interview Date,Interview Start Time,GPS Location,'
-      'Picture Chart Collected,Picture Chart Not Collected Reason,'
-      'Interview End Time,Interview Finished In One Visit,'
-      'Second Interview Visit Date,Second Visit Reason,Interview Outcome,'
-      'Interview Not Completed Reason,Comments,Is Finished,Food Item ID,'
-      'Food Item Name,Food Item Time Period,Food Item Source,'
-      'Ingredients Description,Preparation Method,Confirmed,'
-      'Recipe Number,Recipe Date,Recipe Name,Measurements\n';
-
-  String finishedGibsonsFormsCsv = finishedGibsonsFormsCsvHeader;
-
-  for (GibsonsForm? gibsonsForm in gibsonsForms) {
-    if (gibsonsForm != null && gibsonsForm.finished) {
-      finishedGibsonsFormsCsv += gibsonsForm.toCsv();
-    }
-  }
-  return finishedGibsonsFormsCsv;
-}
-
-// TODO: move this function to the API
-String _convertRecipesToCsv(List<Recipe> recipes) {
-  String recipesCsvHeader =
-      'Employee Number,Recipe Number,Date,Recipe Name,Recipe Type,'
-      'Recipe attribute,Recipe Measurement,Recipe Probe Name,'
-      'Recipe Probe Answers,Ingredient Name,Ingredient Description,'
-      'Cooking State,Ingredient Measurement\n';
-
-  String recipesCsv = recipesCsvHeader;
-
-  for (Recipe recipe in recipes) {
-    recipesCsv += recipe.toCsv();
-  }
-
-  return recipesCsv;
 }
--- packages/gibsonify_api/lib/src/models/utils.dart
+++ packages/gibsonify_api/lib/src/models/utils.dart
@@ -1,4 +1,4 @@
-import 'measurement.dart';
+import 'package:gibsonify_api/gibsonify_api.dart';
 
 /// Checks whether the `String` field is not null and not an empty string, which,
 /// in a user input form field case, indicates that the user has modified the
@@ -62,4 +62,48 @@ String combineMeasurements(List<Measurement> measurements) {
   measurementsCombined = measurementsCombined.substring(
       0, measurementsCombined.length - ' + '.length);
   return measurementsCombined;
+}
+
+/// Converts a `List` of nullable `GibsonsForm` elements into a csv-formatted
+/// `String`.
+String convertFinishedGibsonsFormsToCsv(List<GibsonsForm?> gibsonsForms) {
+  String finishedGibsonsFormsCsvHeader =
+      'Collection ID,Employee Number,Household ID,Respondent Name,'
+      'Respondent Country Code,Respondent Tel Number Prefix,'
+      'Respondent Tel Number,Sensitization Date,Recall Day,'
+      'Interview Date,Interview Start Time,GPS Location,'
+      'Picture Chart Collected,Picture Chart Not Collected Reason,'
+      'Interview End Time,Interview Finished In One Visit,'
+      'Second Interview Visit Date,Second Visit Reason,Interview Outcome,'
+      'Interview Not Completed Reason,Comments,Is Finished,Food Item ID,'
+      'Food Item Name,Food Item Time Period,Food Item Source,'
+      'Ingredients Description,Preparation Method,Confirmed,'
+      'Recipe Number,Recipe Date,Recipe Name,Measurements\n';
+
+  String finishedGibsonsFormsCsv = finishedGibsonsFormsCsvHeader;
+
+  for (GibsonsForm? gibsonsForm in gibsonsForms) {
+    if (gibsonsForm != null && gibsonsForm.finished) {
+      finishedGibsonsFormsCsv += gibsonsForm.toCsv();
+    }
+  }
+  return finishedGibsonsFormsCsv;
+}
+
+/// Converts a `List` of nullable `Recipe` elements into a csv-formatted
+/// `String`.
+String convertRecipesToCsv(List<Recipe> recipes) {
+  String recipesCsvHeader =
+      'Employee Number,Recipe Number,Date,Recipe Name,Recipe Type,'
+      'Recipe attribute,Recipe Measurement,Recipe Probe Name,'
+      'Recipe Probe Answers,Ingredient Name,Ingredient Description,'
+      'Cooking State,Ingredient Measurement\n';
+
+  String recipesCsv = recipesCsvHeader;
+
+  for (Recipe recipe in recipes) {
+    recipesCsv += recipe.toCsv();
+  }
+
+  return recipesCsv;
 }
