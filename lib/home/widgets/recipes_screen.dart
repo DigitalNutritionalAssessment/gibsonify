b18070bbb6cb88d97c8b68bac1d79517c69797ff
Shazril Suhail <s.shazril@gmail.com>
2022-05-11 20:46:51 +0100

    refactor(recipe): remove slidable usage from recipes screen

--- lib/home/widgets/recipes_screen.dart
+++ lib/home/widgets/recipes_screen.dart
@@ -4,7 +4,6 @@ import 'package:gibsonify/navigation/navigation.dart';
 import 'package:gibsonify/recipe/recipe.dart';
 import 'package:gibsonify/login/login.dart';
 import 'package:gibsonify_api/gibsonify_api.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 
 class RecipesScreen extends StatelessWidget {
   final String? assignedFoodItemId;
@@ -28,76 +27,56 @@ class RecipesScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(2.0),
                 itemCount: recipeState.recipes.length,
                 itemBuilder: (context, index) {
-                  return Slidable(
-                    endActionPane: ActionPane(
-                      motion: const ScrollMotion(),
-                      children: [
-                        SlidableAction(
-                          onPressed: (context) => showDialog<String>(
+                  return Card(
+                      child: ListTile(
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 10.0),
+                          title: isFieldNotNullAndNotEmpty(
+                                  recipeState.recipes[index].name)
+                              ? Text(recipeState.recipes[index].name!)
+                              : const Text('Unnamed Recipe'),
+                          subtitle: Text(recipeState.recipes[index].type +
+                              recipeState.recipes[index]
+                                  .ingredientNamesString()),
+                          trailing: recipeState.recipes[index].saved
+                              ? const Icon(Icons.done)
+                              : const Icon(Icons.new_releases),
+                          onTap: () => {
+                                context.read<RecipeBloc>().add(
+                                    RecipeProbesCleared(
+                                        recipe: recipeState.recipes[index])),
+                                Navigator.pushNamed(context, PageRouter.recipe,
+                                    arguments: (recipeState
+                                                .recipes[index].type !=
+                                            'Standard Recipe')
+                                        ? {
+                                            'recipeIndex': index,
+                                            'assignedFoodItemId':
+                                                assignedFoodItemId,
+                                            'foodItemDescription':
+                                                foodItemDescription,
+                                            'selectedScreen':
+                                                SelectedRecipeScreen
+                                                    .ingredientScreen
+                                          }
+                                        : {
+                                            'recipeIndex': index,
+                                            'assignedFoodItemId':
+                                                assignedFoodItemId,
+                                            'foodItemDescription':
+                                                foodItemDescription,
+                                            'selectedScreen':
+                                                SelectedRecipeScreen.probeScreen
+                                          })
+                              },
+                          onLongPress: () => showModalBottomSheet(
                               context: context,
-                              builder: (BuildContext context) =>
-                                  DeleteRecipeDialog(
-                                      recipe: recipeState.recipes[index])),
-                          backgroundColor: Colors.red,
-                          foregroundColor: Colors.white,
-                          icon: Icons.delete,
-                          label: 'Delete',
-                        )
-                      ],
-                    ),
-                    child: Card(
-                        child: ListTile(
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 8.0, horizontal: 10.0),
-                            title: isFieldNotNullAndNotEmpty(
-                                    recipeState.recipes[index].name)
-                                ? Text(recipeState.recipes[index].name!)
-                                : const Text('Unnamed Recipe'),
-                            subtitle: Text(recipeState.recipes[index].type +
-                                recipeState.recipes[index]
-                                    .ingredientNamesString()),
-                            trailing: recipeState.recipes[index].saved
-                                ? const Icon(Icons.done)
-                                : const Icon(Icons.new_releases),
-                            onTap: () => {
-                                  context.read<RecipeBloc>().add(
-                                      RecipeProbesCleared(
-                                          recipe: recipeState.recipes[index])),
-                                  Navigator.pushNamed(
-                                      context, PageRouter.recipe,
-                                      arguments:
-                                          (recipeState.recipes[index].type !=
-                                                  'Standard Recipe')
-                                              ? {
-                                                  'recipeIndex': index,
-                                                  'assignedFoodItemId':
-                                                      assignedFoodItemId,
-                                                  'foodItemDescription':
-                                                      foodItemDescription,
-                                                  'selectedScreen':
-                                                      SelectedRecipeScreen
-                                                          .ingredientScreen
-                                                }
-                                              : {
-                                                  'recipeIndex': index,
-                                                  'assignedFoodItemId':
-                                                      assignedFoodItemId,
-                                                  'foodItemDescription':
-                                                      foodItemDescription,
-                                                  'selectedScreen':
-                                                      SelectedRecipeScreen
-                                                          .probeScreen
-                                                })
-                                },
-                            onLongPress: () => showModalBottomSheet(
-                                context: context,
-                                builder: (context) {
-                                  return RecipeOptions(
-                                      recipe: recipeState.recipes[index],
-                                      employeeNumber:
-                                          loginState.loginInfo.employeeId!);
-                                }))),
-                  );
+                              builder: (context) {
+                                return RecipeOptions(
+                                    recipe: recipeState.recipes[index],
+                                    employeeNumber:
+                                        loginState.loginInfo.employeeId!);
+                              })));
                 }),
             floatingActionButton: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -144,36 +123,6 @@ class RecipesScreen extends StatelessWidget {
                           })
                 ]));
       });
-    });
-  }
-}
-
-class DeleteRecipeDialog extends StatelessWidget {
-  final Recipe recipe;
-
-  const DeleteRecipeDialog({Key? key, required this.recipe}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<RecipeBloc, RecipeState>(builder: (context, state) {
-      return AlertDialog(
-        title: const Text('Delete recipe'),
-        content: Text('Would you like to delete the ${recipe.name} recipe?'),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context, 'Cancel'),
-            child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () => {
-              context.read<RecipeBloc>().add(RecipeDeleted(recipe: recipe)),
-              context.read<RecipeBloc>().add(const RecipesSaved()),
-              Navigator.pop(context, 'Delete')
-            },
-            child: const Text('Delete'),
-          ),
-        ],
-      );
     });
   }
 }
