de2878474b55d429f7f16f45274fdc5f3a2f6172
Adam Sroka <git@adam.sr>
2022-05-17 17:47:01 +0100

    test(collection): initial selected screen
    Preliminary test for initial selected screen being sensitization screen.

---
+++ test/collection/collection_bloc_test.dart
@@ -0,0 +1,34 @@
+import 'package:test/test.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:gibsonify/collection/collection.dart';
+import 'package:gibsonify_repository/gibsonify_repository.dart';
+
+class MockGibsonifyRepository extends Mock implements GibsonifyRepository {}
+
+// TODO:
+// class FakeGibsonsForm extends Fake implements GibsonsForm {}
+
+void main() {
+  group('CollectionBloc', () {
+    late GibsonifyRepository gibsonifyRepository;
+
+    late CollectionBloc collectionBloc;
+
+    setUp(() {
+      gibsonifyRepository = MockGibsonifyRepository();
+
+      collectionBloc = CollectionBloc(gibsonifyRepository: gibsonifyRepository);
+    });
+
+    test('initial selected screen is sensitization screen', () {
+      expect(collectionBloc.state.selectedScreen, SelectedScreen.sensitization);
+    });
+
+    // blocTest(
+    //   'emits [] when nothing is added',
+    //   build: () => CollectionBloc(),
+    //   expect: () => [],
+    // );
+  });
+}
