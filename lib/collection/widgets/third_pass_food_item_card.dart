7ca9173e37fc4cb564105f215c76ab7b143b9a96
Adam Sroka <git@adam.sr>
2022-05-21 14:00:42 +0100

    feat(collection): clear measurement unit and value after method change
    Null measurement unit and value after changing the measurement method. This was initially attempted to be implemented by sending two more events when modifying the method, namely the events to null the unit and value, which yielded a very tricky to find bug related to indexing of FoodItems by indexOf(), which was (partially) fixed to indexing by indexWhere() using UUIDs. This needs to be fully fixed in the future.

--- lib/collection/widgets/third_pass_food_item_card.dart
+++ lib/collection/widgets/third_pass_food_item_card.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-// import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:gibsonify/collection/collection.dart';
 
+import 'package:gibsonify/collection/collection.dart';
 import 'package:gibsonify_api/gibsonify_api.dart';
 
 class ThirdPassFoodItemCard extends StatelessWidget {
   const ThirdPassFoodItemCard(
       {Key? key,
       required this.foodItem,
-      this.onMeasurementMethodChanged,
+      this.onMeasurementMethodChangedOthersNulled,
       this.onMeasurementValueChanged,
       this.onMeasurementUnitChanged,
       this.onMeasurementAdded,
@@ -17,7 +16,8 @@ class ThirdPassFoodItemCard extends StatelessWidget {
       : super(key: key);
 
   final FoodItem foodItem;
-  final ValueChanged<Map<String, dynamic>>? onMeasurementMethodChanged;
+  final ValueChanged<Map<String, dynamic>>?
+      onMeasurementMethodChangedOthersNulled;
   final ValueChanged<Map<String, dynamic>>? onMeasurementUnitChanged;
   final ValueChanged<Map<String, dynamic>>? onMeasurementValueChanged;
   final VoidCallback? onMeasurementAdded;
@@ -72,120 +72,20 @@ class ThirdPassFoodItemCard extends StatelessWidget {
                       ),
                       child: MeasurementCard(
                           measurement: foodItem.measurements[index],
-                          onMeasurementMethodChanged: (measurementMethod) =>
-                              onMeasurementMethodChanged!({
-                                'index': index,
-                                'method': measurementMethod
-                              }),
+                          onMeasurementMethodChangedOthersNulled:
+                              (measurementMethod) =>
+                                  onMeasurementMethodChangedOthersNulled!({
+                                    'index': index,
+                                    'method': measurementMethod
+                                  }),
                           onMeasurementUnitChanged: (measurementUnit) =>
                               onMeasurementUnitChanged!(
                                   {'index': index, 'unit': measurementUnit}),
                           onMeasurementValueChanged: (measurementValue) =>
-                              onMeasurementValueChanged!(
-                                  {'index': index, 'value': measurementValue}))
-                      //     Card(
-                      //   child: Column(
-                      //     children: [
-                      //       DropdownSearch<String>(
-                      //           key: UniqueKey(),
-                      //           popupProps: const PopupProps.menu(
-                      //               showSelectedItems: true,
-                      //               fit: FlexFit.loose,
-                      //               menuProps: MenuProps(
-                      //                   constraints: BoxConstraints.tightFor())),
-                      //           dropdownSearchDecoration: InputDecoration(
-                      //             icon: const Icon(Icons.monitor_weight_outlined),
-                      //             labelText: "Measurement method",
-                      //             helperText: 'How the measurement is measured',
-                      //             // TODO: the errorText should be displayed if nothing is chosen
-                      //             // so investigate how this can be achieved with focusnodes or
-                      //             // mayge send an empty string (although that would not work all
-                      //             // the time)
-                      //             errorText: !foodItem.measurements[index]
-                      //                     .isMethodValid()
-                      //                 ? 'Enter a measurement method'
-                      //                 : null,
-                      //           ),
-                      //           items: Measurement.measurementMethods,
-                      //           // TODO: clear measurement unit and value when
-                      //           // measurement method is changed
-                      //           onChanged: (String? measurementMethod) {
-                      //             onMeasurementMethodChanged!({
-                      //               'index': index,
-                      //               'method': measurementMethod
-                      //             });
-                      //             // clear unit and value after method change
-                      //             // onMeasurementUnitChanged!(
-                      //             //     {'index': index, 'unit': ''});
-                      //             // onMeasurementValueChanged!(
-                      //             //     {'index': index, 'value': ''});
-                      //           },
-                      //           selectedItem: foodItem
-                      //               .measurements[index].measurementMethod),
-                      //       DropdownSearch<String>(
-                      //           key: UniqueKey(),
-                      //           // TODO: Make measurement unit read only if no measurement method is
-                      //           // chosen, and display a snackbar saying to first choose method
-                      //           popupProps: const PopupProps.menu(
-                      //               showSelectedItems: true,
-                      //               fit: FlexFit.loose,
-                      //               menuProps: MenuProps(
-                      //                   constraints: BoxConstraints.tightFor())),
-                      //           dropdownSearchDecoration: InputDecoration(
-                      //             icon: const Icon(
-                      //                 Icons.radio_button_unchecked_outlined),
-                      //             labelText: "Measurement unit",
-                      //             helperText:
-                      //                 'The size of each measurement value',
-                      //             // TODO: the errorText should be displayed if nothing is chosen
-                      //             // so investigate how this can be achieved with focusnodes or
-                      //             // mayge send an empty string (although that would not work all
-                      //             // the time)
-                      //             errorText:
-                      //                 !foodItem.measurements[index].isUnitValid()
-                      //                     ? 'Select the measurement unit'
-                      //                     : null,
-                      //           ),
-                      //           items: foodItem.measurements[index]
-                      //               .getMeasurementUnitsForMethod()
-                      //           // _getMeasurementUnitsForMethod(foodItem
-                      //           //     .measurements[index].measurementMethod)
-                      //           ,
-                      //           onChanged: (String? measurementUnit) =>
-                      //               onMeasurementUnitChanged!({
-                      //                 'index': index,
-                      //                 'unit': measurementUnit
-                      //               }),
-                      //           selectedItem:
-                      //               foodItem.measurements[index].measurementUnit),
-                      //       TextFormField(
-                      //         // key: UniqueKey(),
-                      //         initialValue:
-                      //             foodItem.measurements[index].measurementValue,
-                      //         decoration: InputDecoration(
-                      //           icon: const Icon(
-                      //               Icons.drive_file_rename_outline_rounded),
-                      //           labelText: 'Measurement value or quantity',
-                      //           helperText: 'The amount or number you measured',
-                      //           errorText: !foodItem.measurements[index]
-                      //                   .isValueValid()
-                      //               // TODO: the errorText should be displayed if nothing is typed
-                      //               // so investigate how this can be achieved with focusnodes
-                      //               ? 'Enter the measured value in 1 to 4 digits'
-                      //               : null,
-                      //         ),
-                      //         onChanged: (measurementValue) =>
-                      //             onMeasurementValueChanged!({
-                      //           'index': index,
-                      //           'value': measurementValue
-                      //         }),
-                      //         textInputAction: TextInputAction.next,
-                      //         keyboardType: TextInputType.number,
-                      //       ),
-                      //     ],
-                      //   ),
-                      // ),
-                      );
+                              onMeasurementValueChanged!({
+                                'index': index,
+                                'value': measurementValue
+                              })));
                 }),
             ListTile(
                 title: const Text('Add measurement'),
