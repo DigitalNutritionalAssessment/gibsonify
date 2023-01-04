df1e0bff5467a94d5d627fb66762e2872124681e
Faizaan Pervaiz <fp361@cam.ac.uk>
2023-01-04 19:18:30 +0000

    style(households): inline widget

--- lib/households/view/households.dart
+++ lib/households/view/households.dart
@@ -11,82 +11,70 @@ class HouseholdsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          HouseholdsBloc(isarRepository: context.read<IsarRepository>())
-            ..add(const HouseholdsPageOpened()),
-      child: const HouseholdsView(),
-    );
-  }
-}
-
-class HouseholdsView extends StatelessWidget {
-  const HouseholdsView({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     DateFormat formatter = DateFormat('yyyy-MM-dd');
 
-    return BlocBuilder<HouseholdsBloc, HouseholdsState>(
-      builder: (context, state) {
-        return Scaffold(
-            appBar: AppBar(
-              title: const Text('Households'),
-              actions: [
-                IconButton(
-                    onPressed: () => Navigator.pushNamed(
-                        context, PageRouter.collectionsHelp),
-                    icon: const Icon(Icons.help))
-              ],
-            ),
-            body: Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    padding: const EdgeInsets.all(2.0),
-                    itemCount: state.households.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                          child: ListTile(
-                        title: Text(state.households[index].householdId),
-                        subtitle: Text(formatter
-                            .format(state.households[index].sensitizationDate)),
-                        onTap: () => Navigator.pushNamed(
-                            context,
-                            PageRouter.householdPrefix +
-                                PageRouter.viewHousehold,
-                            arguments: {'id': state.households[index].id}),
-                        onLongPress: () => showModalBottomSheet(
-                            context: context,
-                            builder: (context) {
-                              return HouseholdOptions(
-                                  id: state.households[index].id,
-                                  householdId:
-                                      state.households[index].householdId);
-                            }),
-                      ));
-                    },
-                  ),
-                )
-              ],
-            ),
-            floatingActionButton: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  FloatingActionButton.extended(
-                      heroTag: null,
-                      label: const Text("New household"),
-                      icon: const Icon(Icons.add),
-                      onPressed: () {
-                        Navigator.pushNamed(
-                            context, PageRouter.createHousehold);
-                      }),
-                ]));
-      },
-    );
+    return BlocProvider(
+        create: (context) =>
+            HouseholdsBloc(isarRepository: context.read<IsarRepository>())
+              ..add(const HouseholdsPageOpened()),
+        child: BlocBuilder<HouseholdsBloc, HouseholdsState>(
+          builder: (context, state) {
+            return Scaffold(
+                appBar: AppBar(
+                  title: const Text('Households'),
+                  actions: [
+                    IconButton(
+                        onPressed: () => Navigator.pushNamed(
+                            context, PageRouter.collectionsHelp),
+                        icon: const Icon(Icons.help))
+                  ],
+                ),
+                body: Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        padding: const EdgeInsets.all(2.0),
+                        itemCount: state.households.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                              child: ListTile(
+                            title: Text(state.households[index].householdId),
+                            subtitle: Text(formatter.format(
+                                state.households[index].sensitizationDate)),
+                            onTap: () => Navigator.pushNamed(
+                                context,
+                                PageRouter.householdPrefix +
+                                    PageRouter.viewHousehold,
+                                arguments: {'id': state.households[index].id}),
+                            onLongPress: () => showModalBottomSheet(
+                                context: context,
+                                builder: (context) {
+                                  return HouseholdOptions(
+                                      id: state.households[index].id,
+                                      householdId:
+                                          state.households[index].householdId);
+                                }),
+                          ));
+                        },
+                      ),
+                    )
+                  ],
+                ),
+                floatingActionButton: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      FloatingActionButton.extended(
+                          heroTag: null,
+                          label: const Text("New household"),
+                          icon: const Icon(Icons.add),
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, PageRouter.createHousehold);
+                          }),
+                    ]));
+          },
+        ));
   }
 }
 
