8fb02cdf9c45002c0e9c3108115e2c2caa5fdb95
Faizaan Pervaiz <fp361@cam.ac.uk>
2023-01-04 13:42:34 +0000

    chore(gibsonify_api): remove unused Forms code
    Signed-off-by: Faizaan Pervaiz <fp361@cam.ac.uk>

--- packages/gibsonify_repository/lib/src/gibsonify_repository.dart
+++ packages/gibsonify_repository/lib/src/gibsonify_repository.dart
@@ -9,18 +9,10 @@ class GibsonifyRepository {
   const GibsonifyRepository({required GibsonifyApi gibsonifyApi})
       : _gibsonifyApi = gibsonifyApi;
 
-  Future<void> saveForm(GibsonsForm gibsonsForm) =>
-      _gibsonifyApi.saveForm(gibsonsForm);
-
-  // TODO: investigate providing a Stream of forms instead
-  List<GibsonsForm?> loadForms() => _gibsonifyApi.loadForms();
-
   Future<void> saveLoginInfo(LoginInfo loginInfo) =>
       _gibsonifyApi.saveLoginInfo(loginInfo);
 
   LoginInfo loadLoginInfo() => _gibsonifyApi.loadLoginInfo();
-
-  void deleteForm(String id) => _gibsonifyApi.deleteForm(id);
 
   // TODO: We should think about how this will scale with lots of recipes
   List<Recipe> loadRecipes() => _gibsonifyApi.loadRecipes();
