11ae7aa5999dfd7d37a6974a3f14a432261ddbbb
Archie <archie_l_carpenter@hotmail.com>
2023-02-13 12:08:43 +0000

    feat: sync bloc and parts

---
+++ lib/sync/bloc/sync_event.dart
@@ -0,0 +1,64 @@
+part of 'sync_bloc.dart';
+
+abstract class SyncEvent extends Equatable{
+  const SyncEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CheckLocationPermissions extends SyncEvent {}
+
+class CheckLocationEnabled extends SyncEvent {}
+
+class CheckWifiEnabled extends SyncEvent {}
+
+class AskLocationPermissions extends SyncEvent {}
+
+class EnableLocationServices extends SyncEvent {}
+
+class EnableWifiServices extends SyncEvent {}
+
+class Initialise extends SyncEvent {}
+
+class Register extends SyncEvent {}
+
+class Unregister extends SyncEvent {}
+
+class DiscoverDevices extends SyncEvent {}
+
+class PeersUpdated extends SyncEvent {
+  const PeersUpdated({required this.peers});
+  final List<DiscoveredPeers> peers;
+}
+
+class Connect extends SyncEvent {
+  const Connect({required this.deviceAddress});
+  final String deviceAddress;
+}
+
+class StartSocket extends SyncEvent {
+  const StartSocket({required this.groupOwnerAddress});
+  final String groupOwnerAddress;
+}
+
+class ConnectToSocket extends SyncEvent {
+  const ConnectToSocket({required this.groupOwnerAddress});
+  final String groupOwnerAddress;
+}
+
+class SendStringToSocket extends SyncEvent {
+  const SendStringToSocket({required this.message});
+  final String message;
+}
+
+class GetWiFiP2pInfo extends SyncEvent {}
+
+class UpdateWiFiP2pInfo extends SyncEvent {
+  const UpdateWiFiP2pInfo({required this.wifiP2PInfo});
+  final WifiP2PInfo wifiP2PInfo;
+}
+
+class Init extends SyncEvent {}
+
+class Close extends SyncEvent {}
---
+++ lib/sync/bloc/sync_state.dart
@@ -0,0 +1,79 @@
+part of 'sync_bloc.dart';
+
+enum FoundDevices {
+  found,
+  loading,
+  notFound
+}
+
+class SyncState extends Equatable {
+
+  const SyncState({
+    this.locationPermissionsGranted = false,
+    this.locationEnabled = false,
+    this.wifiEnabled = false,
+    this.error,
+    this.initialised = false,
+    this.registered = false,
+    this.wifiP2pInfo,
+    this.streamPeers,
+    this.peers = const [],
+    this.status = '',
+  });
+
+  final bool locationPermissionsGranted;
+  final bool locationEnabled;
+  final bool wifiEnabled;
+  final String? error;
+  final String status;
+
+  final bool initialised;
+  final bool registered;
+
+  final WifiP2PInfo? wifiP2pInfo;
+  final StreamSubscription<List<DiscoveredPeers>>? streamPeers;
+
+  final List<DiscoveredPeers> peers;
+
+  SyncState copyWith({
+    bool? locationPermissionsGranted,
+    bool? locationEnabled,
+    bool? wifiEnabled,
+    String? error,
+    bool? initialised,
+    bool? registered,
+    WifiP2PInfo? wifiP2PInfo,
+    StreamSubscription<List<DiscoveredPeers>>? streamPeers,
+    List<DiscoveredPeers>? peers,
+    String? status,
+  }) {
+    return SyncState(
+      locationPermissionsGranted: locationPermissionsGranted ?? this.locationPermissionsGranted,
+      locationEnabled: locationEnabled ?? this.locationEnabled,
+      wifiEnabled: wifiEnabled ?? this.wifiEnabled,
+      error: error ?? this.error,
+      initialised: initialised ?? this.initialised,
+      registered: registered ?? this.registered,
+      wifiP2pInfo: wifiP2PInfo ?? this.wifiP2pInfo,
+      streamPeers: streamPeers ?? this.streamPeers,
+      peers: peers ?? this.peers,
+      status: status ?? this.status,
+    );
+  }
+
+
+  @override
+  List<Object?> get props => [
+    locationPermissionsGranted,
+    locationEnabled,
+    wifiEnabled,
+    error,
+    initialised,
+    registered,
+    wifiP2pInfo,
+    streamPeers,
+    peers,
+    status
+  ];
+
+}
