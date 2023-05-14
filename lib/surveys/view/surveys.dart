8b98f987650fd8f719e73f7fd7b3e93bdf7f9fc6
Faizaan Pervaiz <fp361@cam.ac.uk>
2023-05-14 18:07:45 +0100

    feat(surveys): delete logic updates (#90)

--- lib/surveys/view/surveys.dart
+++ lib/surveys/view/surveys.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gibsonify/login/login.dart';
 import 'package:gibsonify/surveys/surveys.dart';
 import 'package:gibsonify_api/gibsonify_api.dart';
+import 'package:gibsonify_repository/gibsonify_repository.dart';
 
 class SurveysScreen extends StatelessWidget {
   const SurveysScreen({Key? key}) : super(key: key);
@@ -168,18 +169,44 @@ class SurveyOptions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> options = [
-      ListTile(title: Text(surveyId)),
-      const Divider(),
-      ListTile(
-        leading: const Icon(Icons.delete),
-        title: const Text('Delete'),
-        onTap: () {
-          context.read<SurveysBloc>().add(SurveyDeleteRequested(id: surveyId));
-          Navigator.pop(context);
-        },
-      )
-    ];
-    return Wrap(children: options);
+    return BlocBuilder<LoginBloc, LoginState>(
+      builder: (context, loginState) {
+        final List<Widget> options = [
+          ListTile(title: Text(surveyId)),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.delete),
+            title: const Text('Delete'),
+            enabled: loginState.loginInfo.employeeIsSupervisor ?? false,
+            onTap: () {
+              final households =
+                  context.read<HiveRepository>().readHouseholds();
+              if (allowSurveyDelete(
+                  surveyId: surveyId, households: households)) {
+                context
+                    .read<SurveysBloc>()
+                    .add(SurveyDeleteRequested(id: surveyId));
+              } else {
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(SnackBar(
+                      content: Text(
+                          'Survey $surveyId cannot be deleted because there are collections associated with it.')));
+              }
+              Navigator.pop(context);
+            },
+          )
+        ];
+        return Wrap(children: options);
+      },
+    );
   }
+}
+
+bool allowSurveyDelete(
+    {required String surveyId, required Iterable<Household> households}) {
+  return !households
+      .expand((household) => household.respondents.values)
+      .expand((respondent) => respondent.collections.values)
+      .any((collection) => collection.surveyId == surveyId);
 }
